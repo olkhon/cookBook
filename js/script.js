fbcd3599997c1bf5f23f1a952c7316dd37711336
olkhon <alexander.stapel@gmail.com>
2020-12-04 11:11:32 +0100

    refactoring hover function headline cards

--- js/script.js
+++ js/script.js
@@ -12,10 +12,6 @@ for (var i = 0; i < button.length; i++) {
     button[i].addEventListener("click", myFunction, false);
 }
 
-
-
-
-
 $(document).ready(function() {
     //  alert('jquery is successfully running');
     $('#sidebarCollapse').on('click', function() {
@@ -25,99 +21,16 @@ $(document).ready(function() {
 });
 
 
-/* functions mouse over displaying h content, stills needs some improvements */
+const cards = document.getElementsByClassName("card");
 
-function displayHeadlineOne() {
-
-    var headline = document.createElement("h5"); // Create h5 element
-    headline.style.color = "black";
-    var headlineText = document.createTextNode("Vegetarian biryani!"); // Create a text node
-    headline.appendChild(headlineText); // Append the text to headline
-    document.getElementById("recipeOne").appendChild(headline);
-}
-
-
-function removeHeadlineOne() {
-    var headlineDiv = document.getElementById("recipeOne");
-    headlineDiv.removeChild(headlineDiv.childNodes[0]);
-}
-
-function displayHeadlineTwo() {
-
-    var headline = document.createElement("h5"); // Create h5 element
-    headline.style.color = "black";
-    var headlineText = document.createTextNode("Chinese eggplant with spicy szechuan sauce"); // Create a text node
-    headline.appendChild(headlineText); // Append the text to headline
-    document.getElementById("recipeTwo").appendChild(headline);
-}
-
-function displayHeadlineThree() {
-
-    var headline = document.createElement("h5"); // Create h5 element
-    headline.style.color = "black";
-    var headlineText = document.createTextNode("Portobello mushroom burger"); // Create a text node
-    headline.appendChild(headlineText); // Append the text to headline
-    document.getElementById("recipeThree").appendChild(headline);
-}
-
-function displayHeadlineFour() {
-
-    var headline = document.createElement("h5"); // Create h5 element
-    headline.style.color = "black";
-    var headlineText = document.createTextNode("Orecchiette with creamy carrot miso sauce"); // Create a text node
-    headline.appendChild(headlineText); // Append the text to headline
-    document.getElementById("recipeFour").appendChild(headline);
-}
-
-function displayHeadlineFive() {
-
-    var headline = document.createElement("h5"); // Create h5 element
-    headline.style.color = "black";
-    var headlineText = document.createTextNode("Vegan tlayudas!"); // Create a text node
-    headline.appendChild(headlineText); // Append the text to headline
-    document.getElementById("recipeFive").appendChild(headline);
-}
-
-
-function displayHeadlineSix() {
-
-    var headline = document.createElement("h5"); // Create h5 element
-    headline.style.color = "black";
-    var headlineText = document.createTextNode("Bali bowls with peanut tofu"); // Create a text node
-    headline.appendChild(headlineText); // Append the text to headline
-    document.getElementById("recipeSix").appendChild(headline);
-}
-
-
-
-/* mouse out functions -> to do */
-
-function removeHeadlineOne() {
-    var headlineDiv = document.getElementById("recipeOne");
-    headlineDiv.removeChild(headlineDiv.childNodes[0]);
-}
-
-function removeHeadlineTwo() {
-    var headlineDiv = document.getElementById("recipeTwo");
-    headlineDiv.removeChild(headlineDiv.childNodes[0]);
-}
-
-function removeHeadlineThree() {
-    var headlineDiv = document.getElementById("recipeThree");
-    headlineDiv.removeChild(headlineDiv.childNodes[0]);
-}
-
-function removeHeadlineFour() {
-    var headlineDiv = document.getElementById("recipeFour");
-    headlineDiv.removeChild(headlineDiv.childNodes[0]);
-}
-
-function removeHeadlineFive() {
-    var headlineDiv = document.getElementById("recipeFive");
-    headlineDiv.removeChild(headlineDiv.childNodes[0]);
-}
-
-function removeHeadlineSix() {
-    var headlineDiv = document.getElementById("recipeSix");
-    headlineDiv.removeChild(headlineDiv.childNodes[0]);
+for (let index = 0; index < cards.length; index++) {
+    const card = cards[index];
+    card.addEventListener("mouseenter", (event) => {
+        const h5Tag = event.target.childNodes[1];
+        h5Tag.style.visibility = "visible";
+    });
+    card.addEventListener("mouseleave", (event) => {
+        const h5Tag = event.target.childNodes[1];
+        h5Tag.style.visibility = "hidden";
+    });
 }
